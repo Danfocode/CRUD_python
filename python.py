a6d721c22291d407aa271591a7fa0a442dcc6e7a
Daniel Orejuela <danielorejuelag@gmail.com>
2025-02-22 19:19:08 -0500

    Creating labels and inputs

--- python.py
+++ python.py
@@ -6,8 +6,28 @@ class Product:
     def __init__(self, window):
         self.wind = window
         self.wind.title('Products application')
+        
+        # creating a frame container
+        frame = LabelFrame(self.wind, text='Register A new Product')
+        frame.grid(row=0, column=0, columnspan=3)
 
-if __name__== '__main__':
+        # ID imput
+        Label(frame, text='ID: '). grid(row=1, column=0)
+        self.name = Entry(frame)
+        self.name.grid(row=1, column=1)
+
+          # Name imput
+        Label(frame, text='Name: '). grid(row=2, column=0)
+        self.name = Entry(frame)
+        self.name.grid(row=2, column=1)
+
+         # City imput
+        Label(frame, text='City: '). grid(row=3, column=0)
+        self.name = Entry(frame)
+        self.name.grid(row=3, column=1)
+
+
+if __name__ == '__main__':
     window = Tk()
     application = Product(window)
     window.mainloop()
